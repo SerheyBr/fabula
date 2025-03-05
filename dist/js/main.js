7776247db70190772318083b2be39b87654fa80c
SerheyBr <sergeydrilkov@mail.ru>
2025-03-05 14:01:00 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+
+
+//проверка работы слайдера
+const swiper = new Swiper('.mySlider', {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+})
+
+//# sourceMappingURL=main.js.map
